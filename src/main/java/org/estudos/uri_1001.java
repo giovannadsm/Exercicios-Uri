f7a68aaae5ebec2b05e47535e35f75a152545032
Giovanna <giovannadsmessias@outlook.com>
2023-01-05 19:27:44 -0300

    Exercício - 1001
    Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".

---
+++ src/main/java/org/estudos/uri_1001.java
@@ -0,0 +1,17 @@
+package org.estudos;
+
+import java.util.Scanner;
+
+public class uri_1001 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = A + B;
+
+        System.out.println("X = " + C);
+
+    }
+}
