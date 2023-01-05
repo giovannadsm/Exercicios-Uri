09028a60174d211251bf0e3d2585aa6677dcb230
Giovanna <giovannadsmessias@outlook.com>
2023-01-04 21:39:24 -0300

    Commit inicial

--- src/main/java/org/estudos/Main.java
+++ src/main/java/org/estudos/Main.java
@@ -1,4 +1,6 @@
-package org.estudos;public class Main {
+package org.estudos;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
