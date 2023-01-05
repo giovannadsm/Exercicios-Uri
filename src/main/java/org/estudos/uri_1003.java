c629be1c156643e1ccb551baefb7568c90bc9c1a
Giovanna <giovannadsmessias@outlook.com>
2023-01-05 19:41:20 -0300

    Exercicio - 1003
    Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.

---
+++ src/main/java/org/estudos/uri_1003.java
@@ -0,0 +1,16 @@
+package org.estudos;
+import java.util.Scanner;
+
+public class uri_1003 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = A + B ;
+
+        System.out.println("SOMA = " + C);
+
+    }
+}
