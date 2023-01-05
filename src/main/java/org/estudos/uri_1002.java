f97c02cb544d18aa2c19ba2ec04b7e9af72cd717
Giovanna <giovannadsmessias@outlook.com>
2023-01-05 19:44:47 -0300

    Removendo Format da String exercicio 1002

--- src/main/java/org/estudos/uri_1002.java
+++ src/main/java/org/estudos/uri_1002.java
@@ -6,10 +6,9 @@ public class uri_1002 {
 
     public static void main(String[] args) {
 
-        
             Scanner scanner = new Scanner(System.in);
             double raio = scanner.nextDouble();
 
-            System.out.println(String.format("A=%.4f", 3.14159 * (raio * raio)));
+            System.out.printf("A=%.4f%n", 3.14159 * (raio * raio));
     }
 }
