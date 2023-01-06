497ae3b12b16bb703b651284e6fcaaa1daa4bf11
Giovanna <giovannadsmessias@outlook.com>
2023-01-06 20:40:21 -0300

    Exercício - 1004
    Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.

---
+++ src/main/java/org/estudos/uri_1004.java
@@ -0,0 +1,16 @@
+package org.estudos;
+
+import java.util.Scanner;
+
+public class uri_1004 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int Prod = A * B;
+
+        System.out.println("PROD = " + Prod);
+    }
+}
