89db5720bc8ab8822228fe4ce94fea58641ce798
Giovanna <giovannadsmessias@outlook.com>
2023-01-10 11:05:42 -0300

    Exercício - 1016
    Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
    
    Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
    
    Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.

---
+++ src/main/java/org/estudos/uri_1016.java
@@ -0,0 +1,15 @@
+package org.estudos;
+import java.util.Scanner;
+
+public class uri_1016 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int minutos = 0;
+        int resultado = 0;
+        minutos = sc.nextInt();
+
+        resultado = 2 * minutos;
+        System.out.println(resultado + " minutos");
+    }
+}
