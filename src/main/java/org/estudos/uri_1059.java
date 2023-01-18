85b05ba50bffa87d36438bc4e480a24fae71cf5c
Giovanna <giovannadsmessias@outlook.com>
2023-01-17 23:00:25 -0300

    Exercício - 1059
    Faça um programa que mostre os números pares entre 1 e 100, inclusive.

---
+++ src/main/java/org/estudos/uri_1059.java
@@ -0,0 +1,18 @@
+package org.estudos;
+
+public class uri_1059 {
+
+    public static void main(String[] args) {
+        int quantidade = 100;
+
+        for (int o = 1; o <= quantidade; o++) {
+
+            if (o % 2 == 0) {
+
+                System.out.println(o);
+            }
+        }
+
+    }
+
+}
