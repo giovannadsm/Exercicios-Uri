80b7860408a7d87c7c3abf012c07696783793d0e
Giovanna <giovannadsmessias@outlook.com>
2023-01-24 21:03:02 -0300

    Exercício - 1060
    Faça um programa que leia 6 valores. Estes valores serão somente negativos ou positivos (desconsidere os valores nulos). A seguir, mostre a quantidade de valores positivos digitados.

---
+++ src/main/java/org/estudos/uri_1060.java
@@ -0,0 +1,18 @@
+package org.estudos;
+
+import java.util.Scanner;
+
+public class uri_1060 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int cont = 0;
+        for (int i=0; i < 6; i++) {
+            double n = sc.nextDouble();
+            if (n > 0) cont++;
+        }
+        System.out.println(cont + " valores positivos");
+    }
+}
